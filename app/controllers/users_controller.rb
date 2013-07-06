81c682de5c68a04477939bfc34b716ad68655086
koria1 <koria1.nku.edu>
2013-07-06 07:09:39 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  def show
+	before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy 
+ def show
     @user = User.find(params[:id])
   end
 
@@ -17,5 +20,57 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+
+#=begin
+ def edit
+    @user = User.find(params[:id])
+  end
+#=end
+
+
+#=begin
+	def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+    	flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+		else
+      render 'edit'
+    end
+  end
+#=end
+
+	def index
+#		@users = User.all
+		@users = User.paginate(page: params[:page])
+	end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
+#=begin
+	private
+
+    def signed_in_user
+		 unless signed_in?
+        store_location 
+     		redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  	 end  
+	end
+
+		def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+#=end
+
+	def admin_user
+      redirect_to(root_path) unless current_user.admin?
   end
 end
